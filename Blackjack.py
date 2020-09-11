d8187ce0f6921864f190dae951b57746b8c10b32
unknown <jonesmat@live.co.uk>
2020-09-11 01:35:19 +0100

    (Work in progress)

--- Blackjack.py
+++ Blackjack.py
@@ -31,39 +31,13 @@ class Shoe:
         return self.shoe.pop()
 
 
-class Bank:
-
-    def __init__(self, name, balance):
-        self.balance = balance
-        self.name = name
-
-    def bet(self):
-        while True:
-            try:
-                amount = int(input('How much would you like to bet: '))
-                if amount < 0:
-                    print("You must enter a positive number!")
-                    while amount <= 0:
-                        amount = int(input('How much would you like to bet: '))
-                if amount <= bank.balance:
-                    self.balance = self.balance - amount
-                    return amount
-                elif amount > self.balance:
-                    print("You do not have enough in your bank for that bet!")
-                else:
-                    print('That is not a valid bet!')
-            except ValueError:
-                print("This is not a valid bet, try entering a number!")
-
-    def winnings(self, active_pot):
-        self.balance = self.balance + active_pot + active_pot
-
-
 class Player:
 
-    def __init__(self, name):
+    def __init__(self, name, balance=1000):
         self.name = name
         self.hand = []
+        self.balance = balance
+        self.stake = 0
 
     def add_cards(self, new_cards):
         self.hand.append(new_cards)
@@ -82,17 +56,59 @@ class Player:
         else:
             return card_values
 
+    def play_hand(self):
+        player_hand_on = True
+        while player_hand_on:
+            if self.true_hand_value() == 21:
+                print("You have 21!")
+                break
+            elif 0 < self.true_hand_value() < 21:
+                player_decision = input("Would you like to draw another card?: ")
+                if player_decision.upper() == 'Y':
+                    self.add_cards(new_game.shoe.deal_one())
+                    for card_in_hand in self.hand:
+                        print(card_in_hand)
+                else:
+                    break
+            else:
+                break
+
+    def bet(self):
+        while True:
+            try:
+                amount = int(input('How much would you like to bet: '))
+                if amount < 0:
+                    print("You must enter a positive number!")
+                    while amount <= 0:
+                        amount = int(input('How much would you like to bet: '))
+                if amount <= self.balance:
+                    self.balance = self.balance - amount
+                    self.stake = amount
+                elif amount > self.balance:
+                    print("You do not have enough in your bank for that bet!")
+                else:
+                    print('That is not a valid bet!')
+            except ValueError:
+                print("This is not a valid bet, try entering a number!")
+
+    def winnings(self):
+        print(f"Congratulations {self.name}! You have won double your stake!")
+        self.balance = self.balance + self.stake + self.stake
+
 
 class Dealer(Player):
 
     def __init__(self, name):
-        super().__init__(name)
+        super().__init__(name, balance=0)
         self.name = name
         self.hand = []
 
     def play_hand(self):
+        print(f"{self.name}'s Turn")
+        for dealer_cards in self.hand:
+            print(dealer_cards)
         while 1 < self.true_hand_value() < 17:
-            self.add_cards(new_shoe.deal_one())
+            self.add_cards(new_game.shoe.deal_one())
             for dealer_cards in self.hand:
                 print(dealer_cards)
         if self.true_hand_value() <= 21:
@@ -100,6 +116,68 @@ class Dealer(Player):
         else:
             print("Dealer is Bust!!")
             return 0
+
+
+class Blackjack:
+
+    def __init__(self):
+        self.shoe = Shoe()
+        self.dealer = Dealer(Dealer)
+        self.table = []
+
+    def remove_players(self):
+        removal = input("Would any players like to stand up? (Y/N): ")
+        while removal.upper() == "Y":
+            player_removal = int(input("Which player would like to stand up? (Seat Number): "))
+            self.table.pop(player_removal)
+            removal = input("Would another player like to stand up? (Y/N): ")
+
+    def add_player(self, player_object):
+        if len(self.table) < 6 and isinstance(player_object, Player):
+            self.table += player_object
+        elif len(self.table) < 6 and not isinstance(player_object, Player):
+            print("Sorry, this is not a player!")
+            create_player = input("Would you like to create this player? (Y/N): ")
+            if create_player.upper() == 'Y':
+                self.table += Player(player_object, 1000)
+        else:
+            print("Sorry, the table is full!")
+
+    def play_round(self):
+        for n in range(2):
+            self.dealer.add_cards(new_game.shoe.deal_one())
+        for players in self.table:
+            for n in range(2):
+                players.add_cards(new_game.shoe.deal_one())
+        print(f"Dealer's Hand: Unknown Card, {new_game.dealer.hand[0]}")
+        for players in self.table:
+            players.bet()
+        for players in self.table:
+            players.play_hand()
+        self.dealer.play_hand()
+        for players in self.table:
+            if win_check(players.true_hand_value(), self.dealer.true_hand_value()):
+                players.winnings()
+        for players in self.table:
+            players.hand = []
+        self.dealer.hand = []
+
+    def play_game(self):
+        print("Welcome to Blackjack!")
+        number_of_players = int(input("how many players would like to play?: "))
+        for size in range(number_of_players):
+            name = input("what is the name of the player?: ")
+            self.add_player(name)
+        self.shoe.shuffle()
+        play_again = True
+        while play_again:
+            self.play_round()
+            self.remove_players()
+            replay = input("Would you like to play another hand? (Y/N): ")
+            if replay.upper() == 'Y':
+                pass
+            else:
+                break
 
 
 def win_check(player_score, dealer_score):
@@ -114,51 +192,5 @@ def win_check(player_score, dealer_score):
 
 
 player = Player('Matt')
-dealer = Dealer('Dealer')
-bank = Bank("Matt's Bank", 500)
-new_shoe = Shoe()
-new_shoe.shuffle()
-game_on = True
-print("Let's play some Blackjack!")
-print(f"{bank.name} contains: {bank.balance}")
-while game_on:
-    for x in range(2):
-        player.add_cards(new_shoe.deal_one())
-        dealer.add_cards(new_shoe.deal_one())
-    print(f"Dealer's Hand: Unknown Card, {dealer.hand[0]}")
-    print(f"Your Cards: {player.hand[0]},{player.hand[1]}")
-    pot = bank.bet()
-    print(f"Pot = {pot}")
-    print(f"Your hand value is {player.true_hand_value()}")
-    player_hand_on = True
-    while player_hand_on:
-        if player.true_hand_value() == 21:
-            print("You have 21!")
-            break
-        elif 0 < player.true_hand_value() < 21:
-            decision = input("Would you like to draw another card?: ")
-            if decision.upper() == 'Y':
-                player.add_cards(new_shoe.deal_one())
-                for card in player.hand:
-                    print(card)
-            else:
-                break
-        else:
-            break
-    print(f"Dealer's Hand: {dealer.hand[1]}, {dealer.hand[0]}")
-    dealer.play_hand()
-    print(f" The value of {player.name}'s hand is {player.true_hand_value()}")
-    print(f" The value of the Dealer's hand is {dealer.true_hand_value()}")
-    if win_check(player.true_hand_value(), dealer.true_hand_value()):
-        print(f"Well Done {player.name}! You have beat the dealer!")
-        bank.winnings(pot)
-    else:
-        print(f"The Dealer has Won!! Better luck next time, {player.name}!")
-    print(f"{bank.name} contains: {bank.balance}")
-    play_again = input("Would you like to play another hand? (Y/N): ")
-    if play_again.upper() == 'Y':
-        player.hand = []
-        dealer.hand = []
-    else:
-        game_on = False
-        print("Thanks for playing!!")
+new_game = Blackjack()
+new_game.play_game()
